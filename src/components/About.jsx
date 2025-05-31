63088d926247b71802b9d86752351ff4cc486942
Uttam Kumar Saha <uttam.saha2019@gift.edu.n>
2025-05-31 10:33:37 +0530

    Resume link added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -43,7 +43,7 @@ const About = () => {
           </p>
 
           <a 
-          href=''
+          href='https://drive.google.com/file/d/1wMhhc-v3wqu6xWUQsE22Au9xqiGjV-i7/view?usp=sharing'
           target='_blank'
           rel='noopener noreferrer'
           className='inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105'
